85018ccad9ffe2155ca8b579ea047bdb44f3ca78
Prachi <prachikale136@gmail.com>
2023-06-29 11:10:44 +0530

    [Prachi] Add: find sequence of lowercase letter joined bunderscore

--- RegexProblems/RegexProblems/Program.cs
+++ RegexProblems/RegexProblems/Program.cs
@@ -16,7 +16,7 @@ namespace RegexProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nEnter option to choose \n1.Check String \n2.Exit");
+                Console.WriteLine("\nEnter option to choose \n1.Check String \n 2 Sequence of lowercase\n3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -24,6 +24,10 @@ namespace RegexProblems
                         Console.WriteLine("\nEnter string that has an ‘a’ followed by two to three 'b'");
                         string str = Console.ReadLine();
                         validate.validateString(str);
+                        break;
+                    case 2:
+                        string lowerCase = "abcde_fghij_klmno_pqrst_uvwx_yz";
+                        validate.validateLowerCaseString(lowerCase);
                         break;
                     default:
                         flag = false;
--- RegexProblems/RegexProblems/Validate.cs
+++ RegexProblems/RegexProblems/Validate.cs
@@ -10,11 +10,24 @@ namespace RegexProblems
     internal class Validate
     {
         public string pattern = @"ab{2,3}";
-        public void validateString(string fName)
+        public string sequence = @"[a-z]+_[A-Z]+";
+        public void validateString(string str)
         {
-            if (Regex.IsMatch(fName, pattern))
+            if (Regex.IsMatch(str, pattern))
             {
-                Console.WriteLine("Input string is correct : " + fName);
+                Console.WriteLine("Input string is correct : " + str);
+            }
+            else
+            {
+                Console.WriteLine("Input string is incorrect");
+            }
+        }
+
+        public void validateLowerCaseString(string lowerCase)
+        {
+            if (Regex.IsMatch(lowerCase, pattern))
+            {
+                Console.WriteLine("Input string is correct : " + lowerCase);
             }
             else
             {
